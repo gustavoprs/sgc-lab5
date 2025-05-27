a6146f2bd0ef50127714c7143c3eb48bdc8e705e
Gustavo Martins <2023019929@aluno.osorio.ifrs.edu.br>
2025-05-27 18:21:18 -0300

    Adicionando arquivo Main.java, principal do projeto.

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {    
+    public static void main(String[] args) {
+        int e = -1;
+        Scanner in = new Scanner(System.in);
+        do {
+            System.out.println("\nBoa tarde?\n\t[1] Boa tarde!\n\t[2] Boa noite, na verdade.\n\t[3] Bom dia, afinal qualquer momento do dia ainda é o dia.\n\n\t[0] Tchau.");
+            try {
+                e = in.nextInt();
+            } catch (InputMismatchException exception) {
+                System.out.println("\nERRO: Só pode número, escolhe uma das opções!");
+                in.nextLine();
+                e = -1;
+            }
+            switch (e) {
+                case 0:
+                    System.out.println("\nTchau então :/\n");
+                    break;
+                case 1:
+                    System.out.println("\nMas no fim pode ser bom dia né, afinal qualquer momento do dia ainda é o dia.");
+                    break;
+                case 2:
+                    System.out.println("\nMas tu não acha que podia ser bom dia também? No fim, qualquer momento do dia ainda é o dia.");
+                    break;
+                case 3:
+                    System.out.println("\nEssa é a ideia certa, exatamente!!\nJá que tu sacou já, vou indo nessa...");
+                    e = 0;
+                    break;
+                default:
+                    break;
+            }
+        } while (e != 0);
+
+        System.exit(0);
+    }
+}
